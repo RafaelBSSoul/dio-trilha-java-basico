a9ba58a24139f254c3c8ed075593e8d15d9bd6e6
RAFAEL BARBOSA <coffeemonstersandbooks@gmail.com>
2025-02-08 08:38:43 -0300

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,10 +2,23 @@ import java.util.Scanner;
 
 public class ContaTerminal {
 public static class Banco {
-    String nomeBanco = "caiva ergonomica sideral";
+    String nomeBanco = "caiva egonomica sideral";
     String agencia;
     String nomeCliente;
     int numero;
+    double saldo;
+    
+
+    public double depositar (double deposito){
+        
+        return saldo += deposito;
+    }
+    public double sacar(double saque){
+        
+        return saldo -= saque;//saque
+    }
+
+
 }
 
     public static void main(String[] args) throws Exception {
@@ -21,7 +34,7 @@ public static class Banco {
             novoBanco.numero = entrada.nextInt();
             if (novoBanco.numero==1021) {
                 System.out.println("O numero da sua conta:"+ novoBanco.numero);
-                novoBanco.nomeCliente = "MARIO ANDRADE";
+                novoBanco.nomeCliente = "MARIO ANDRADE ";
             }
         }
         System.out.println("_______________*******________________");
@@ -30,6 +43,36 @@ public static class Banco {
         System.out.println("-NUMERO DA CONTA: "+novoBanco.numero);
 
         System.out.println("_______________*******________________");
+        int operacao;
+
+        System.out.println("Operações");
+        System.out.println("1-Saldo");
+        System.out.println("2-Sacar");
+        System.out.println("3-Depositar");
+operacao= entrada.nextInt();
+        switch (operacao) {
+            case 1:
+            novoBanco.sacar(entrada.nextDouble());
+            System.out.println(novoBanco.saldo);
+
+                
+                break;
+                 case 2:
+                 novoBanco.depositar(entrada.nextDouble());
+                
+                 System.out.println(novoBanco.saldo);
+
+
+
+                
+                    break;
+                       case 3:
+System.out.println(novoBanco.saldo);                        break;
+        
+            default:
+                break;
+        }
+
 
 
         entrada.close();
